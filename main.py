8376b8dedda21cc8b0c9fd805e90a0f5c359e5af
David Grau <fe80grau@gmail.com>
2023-03-20 20:16:47 +0100

    testing

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ import os
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-#Define Radarr setting and enpoint url constructor
+#Define Radarr setting and enpoint url constructor 
+
+#Queue
 apikey_radarr = config['radarr']['api_key']
 baseurl_radarr = "http://{}:{}/api/v3/".format(config['radarr']['host'], config['radarr']['port'])
 endpoint_radarr = "queue"
@@ -40,8 +42,10 @@ params_radarr = urllib.parse.urlencode({
 })
 
 #Make url merging chunks
-url_radarr = baseurl_radarr + endpoint_radarr + "?" + params_radarr
+queue_url_radarr = baseurl_radarr + endpoint_radarr + "?" + params_radarr
 
+#Get list of all movies /api/v3/movie amd merging history
+movies_history = []
 
 def rename(title, year, quality, file):
     return "{} ({}) {}.{}".format(title, year, quality, file.split('.')[-1])
@@ -49,7 +53,7 @@ def rename(title, year, quality, file):
 
 if __name__ == "__main__":
     #Call url_radarr with GET 
-    data = requests.get(url_radarr).json()
+    data = requests.get(queue_url_radarr).json()
 
     print("Rectificarr is running...")
 
@@ -57,26 +61,42 @@ if __name__ == "__main__":
     #An activity is a download/rewrite process. A movie can be download many times with differents torrent tyrs, each download is an activity.
     if 'records' in data:
         print('Processing data from Radarr')
-        print('{}'.format(url_radarr))
+        print('{}'.format(queue_url_radarr))
 
         #Loop Activies
         for item in data['records']:
-            if 'movieId' in item:
-                #Check if statusMessages is setted. If not, this code will initilice a fake values to avoid errors and keep minimal code
-                if len(item['statusMessages']) < 1:
-                    item['statusMessages'] = [{'messages'  : [''], 'title':''}]
+
+            #Check if statusMessages is setted. If not, this code will initilice a fake values to avoid errors and keep minimal code
+            if len(item['statusMessages']) < 1:
+                item['statusMessages'] = [{'messages'  : [''], 'title':''}]
+
+            trackedDownloadStatus = item['trackedDownloadStatus']
+            trackedDownloadState = item['trackedDownloadState']
+            statusMessage = item['statusMessages'][0]['messages'][0]  
+
+            if not "wasn't grabbed by Radarr" in statusMessage:
+                if not 'movieId' in item:
+                    #Search downloadId in movies_history
+                    #si lo encuentra
+                    #consutrir 
+                        #item['movieId']
+                        #item['movie']['title']
+                        #item['movie']['year']
+                        #item['movie']['path']
+                        #item['quality']['quality']['name']
+
+                    #si no
+                    #break
+                    
 
                 #Getting data from Activity
-                trackedDownloadStatus = item['trackedDownloadStatus']
-                trackedDownloadState = item['trackedDownloadState']
-                statusMessage = item['statusMessages'][0]['messages'][0]
                 movieId = item['movieId']
                 title = item['movie']['title']
                 year = str(item['movie']['year'])
                 path = item['movie']['path']
                 quality = item['quality']['quality']['name']
                 source = item['outputPath']
-                file = item['statusMessages'][0]['title']
+                file_source = item['statusMessages'][0]['title']
 
                 #Print movie title to debug
                 print("||||| {}".format(title))
@@ -84,14 +104,14 @@ if __name__ == "__main__":
                 #Check conditions to ensure that "Unable to parse file" error in statusMessage depends to importPending trackedDownloadState
                 if trackedDownloadStatus == 'warning' \
                 and trackedDownloadState == 'importPending' \
-                and (statusMessage == 'Unable to parse file' or statusMessage == 'Unknown Movie') :
+                and (statusMessage == 'Unable to parse file') :
                     #Print movieId and absolute path to debug
-                    print("|||||||||| Movie warning detectect: {} - {}/{}".format(movieId, title, file))
+                    print("|||||||||| Movie warning detectect: {} - {}/{}".format(movieId, title, file_source))
                     print("- Trying to rename and move to correct folder")
                     #Trying to make a new_name and rewrite in source. After that, Radarr automaticaly will detect this change and can parse the file.
                     try:
-                        new_name = rename(title, year, quality, file)
-                        print("- Old name: {}".format(file))
+                        new_name = rename(title, year, quality, file_source)
+                        print("- Old name: {}".format(file_source))
                         print("- New name: {}".format(new_name))
 
                         #Quick patch to solve https://github.com/fe80Grau/Rectificarr/issues/2
@@ -99,10 +119,14 @@ if __name__ == "__main__":
                             mv_source = source
                             mv_new = source.replace(source.split('/')[-1], new_name)
                         else:
-                            mv_source = source + "/" + file
+                            mv_source = source + "/" + file_source
                             mv_new = source + "/" + new_name
 
                         shutil.move(mv_source, mv_new)
+
+                        #Copy file to Radarr defined movie path
+
+                        #Rescan movie with /api/command?name=RescanMovie?movieId={movieId}
                     except Exception:
                         traceback.print_exc()
     #If endpoints fails...          

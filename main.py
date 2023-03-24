a858ff45f1725d4e8ea626c2a7a80a54597b60b0
David Grau <fe80grau@gmail.com>
2023-03-24 20:22:37 +0100

    fix mkdir and queue delete request

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ if __name__ == "__main__":
                         #Copy file to Radarr defined movie path
                         print("|||||Importing movie... ")
                         if not os.path.isdir(path):
-                            os.mkdir(path.decode('unicode_escape'))
+                            os.mkdir(path.encode("utf8"))
 
                         if not os.path.isfile("{}/{}".format(path, new_name)):
                             shutil.copy(mv_source, "{}/{}".format(path, new_name))
